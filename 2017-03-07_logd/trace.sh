4a44e83be436db94c2f3934a1f4fa1b6c8b8ef1f
Kidd Dawny Lu <huangxiaolu@smartisan.com>
2017-03-08 20:35:01 +0800

    update
    Signed-off-by: Kidd Dawny Lu <huangxiaolu@smartisan.com>

--- 2017-03-07_logd/trace.sh
+++ 2017-03-07_logd/trace.sh
@@ -2,30 +2,29 @@
 
 #set -x
 
-ps="sec0w"
+function strace_ps() {
+	local ps="$1"
 
-while [[ 1 ]]; do
-    line=`adb shell ps | grep $ps`
-    if [[ $? == "0" ]]; then
-        pid=`echo "$line" | awk '{print $2}'`
-        echo "$ps pid is $pid"
-        break
-    fi
-    sleep 0.05
-done
+	while [[ 1 ]]; do
+		line=`adb shell ps | grep $ps`
+		if [[ $? == "0" ]]; then
+			local pid=`echo "$line" | awk '{print $2}'`
+			echo "$ps pid is $pid"
+			break
+		fi
+		#sleep 0.05
+	done
+	adb shell strace -v -f -p $pid
+}
 
-adb shell strace -p $pid
+adb root
+adb remount
+adb push strace /system/bin/ 
+adb shell setenforce 0
 
-ps="dirtyc0w"
+strace_ps "logd" > logd.log &
+strace_ps "krmain" > krmain.log &
+strace_ps "sec0w64" > sec0w64.log & 
+strace_ps "dirtyc0w64" > dirtyc0w64.log &
 
-while [[ 1 ]]; do
-    line=`adb shell ps | grep $ps`
-    if [[ $? == "0" ]]; then
-        pid=`echo "$line" | awk '{print $2}'`
-        echo "$ps pid is $pid"
-        break
-    fi
-    sleep 0.05
-done
-
-adb shell strace -p $pid
+sleep 100
